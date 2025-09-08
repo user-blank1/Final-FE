a3d6603c1ede14b9036151bc3507a28db2c2908c
user-blank1 <linaszilinskasas@gmail.com>
2025-09-08 20:17:11 +0300

    initialise router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const pages = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Main Layout</div>,
+        children: [
+            // { index: true, element: <MainPage /> },
+        ],
+    },
 
+    //   {
+    //     path: "/admin",
+    //     element: <AdminLayout />,
+    //     children: [
+    //       { index: true, element: <Home /> },
+    //       { path: "*", element: <Klaida /> },
+    //     ],
+    //   },
+]);
 function App() {
-    return <></>;
+    return <RouterProvider router={pages} />;
 }
 
 export default App;
